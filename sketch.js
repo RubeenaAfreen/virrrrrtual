dfbdcb0bdeffcc088ba0fe9accd07e1af9f4d70d
RubeenaAfreen <rubinafreen26@gmail.com>
2021-03-25 12:20:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,102 @@
+var dog, sadDog, happyDog;
+var foodObj;
+var fedTime, lastFed;
+var feedBottle, feedbot;
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+  feedBot = loadImage("Images/Milk.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+
+  database = firebase.database();
+
+  foodObj = new Food();
+  
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  feedBottle = createSprite(720, 210, 70, 80);
+  feedBottle.addImage(feedBot);
+  feedBottle.scale=0.1;
+  feedBottle.visible=false
+
+  var feed = createButton("Feed Him");
+  feed.position(600, 100);
+  feed.mousePressed(feedDog);
+
+  var addFood = createButton("Add Food For Him ");
+  addFood.position(680, 100);
+  // addFood.style('font-size', );
+  addFood.mousePressed(addFoods);
+
+
+
+}
+
+function draw() {
+  background(46,139,87);
+  
+  foodObj.display();
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  
+  if(lastFed>=12){
+    textSize(16);
+    text("Last Fed: " + lastFed + "AM", 200, 30);
+  }else if(lastFed == 0){
+    textSize(16);
+    text("Last Fed: 12AM", 200, 30);
+  }else{
+    textSize(16);
+    text("Last Fed: " + lastFed + "PM", 200, 30);
+  }
+
+  
+  drawSprites();
+}
+
+function readStock(data){
+  foods=data.val();
+  foodObj.updateFoodStock(foods);
+}
+
+function feedDog(){
+  dog.addImage(happyDog)
+
+  feedBottle.visible = true;
+  
+
+  if(foodObj.getFoodStock() <= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+function addFoods(){
+  foods++;
+  database.ref('/').update({
+    Food:foods
+    
+  
+  })
+  feedBottle.visible = false;
+
+  }
